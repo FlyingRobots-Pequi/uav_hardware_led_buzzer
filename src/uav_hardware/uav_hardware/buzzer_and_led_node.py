e318226ed2849161bc529e983dbd20e7cfcde9a4
Pedro Lemes <113155395+code-pedrolsl@users.noreply.github.com>
2025-07-14 17:24:47 -0300

    Update buzzer_and_led_node.py

--- src/uav_hardware/uav_hardware/buzzer_and_led_node.py
+++ src/uav_hardware/uav_hardware/buzzer_and_led_node.py
@@ -2,6 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from gpiozero import LED, Buzzer
+from std_msgs.msg import Bool
 
 class BuzzerLEDNode(Node):
     def __init__(self):
@@ -9,57 +10,40 @@ class BuzzerLEDNode(Node):
         self.led = LED(13)
         self.buzzer = Buzzer(21)
 
-        self.step = 0
-        self.state = 'drone_ligado_on'
-        self.timer = self.create_timer(0.2, self.run_pattern)
+        self.led_subscriber = self.create_subscription(
+            Bool,
+            'led',
+            self.led_callback,
+            10
+        )
 
-        self.counter = 0
+        self.buzzer_subscriber = self.create_subscription(
+            Bool,
+            'buzzer',
+            self.buzzer_callback,
+            10
+        )
 
-    def run_pattern(self):
-        if self.state == 'drone_ligado_on':
+        self.get_logger().info("Nó iniciado. Aguardando mensagens nos tópicos '/led' e '/buzzer'.")
+
+    def led_callback(self, msg):
+        if msg.data:
             self.led.on()
+            self.get_logger().info("LED ligado")
+        else:
+            self.led.off()
+            self.get_logger().info("LED desligado")
+
+    def buzzer_callback(self, msg):
+        if msg.data:
             self.buzzer.on()
-            self.state = 'drone_ligado_off'
-            self.counter += 1
-        elif self.state == 'drone_ligado_off':
-            self.led.off()
+            self.get_logger().info("Buzzer ligado")
+        else:
             self.buzzer.off()
-            if self.counter < 10:
-                self.state = 'drone_ligado_on'
-            else:
-                self.state = 'espera_2s'
-                self.counter = 0
-            self.counter += 1
-        elif self.state == 'espera_2s':
-            # Espera 2s = 10 passos de 0.2s
-            self.led.off()
-            self.buzzer.off()
-            if self.counter >= 10:
-                self.state = 'drone_desligado_on'
-                self.counter = 0
-            else:
-                self.counter += 1
-        elif self.state == 'drone_desligado_on':
-            self.led.off()
-            self.buzzer.on()
-            if self.counter >= 7:  # 1.5s = 7 * 0.2s (aprox)
-                self.state = 'espera_5s'
-                self.buzzer.off()
-                self.counter = 0
-            else:
-                self.counter += 1
-        elif self.state == 'espera_5s':
-            self.led.off()
-            self.buzzer.off()
-            if self.counter >= 25:  # 5s = 25 * 0.2s
-                self.state = 'drone_ligado_on'
-                self.counter = 0
-            else:
-                self.counter += 1
+            self.get_logger().info("Buzzer desligado")
 
     def destroy_node(self):
         self.get_logger().info('Encerrando nó...')
-        self.timer.cancel()
         self.led.off()
         self.buzzer.off()
         super().destroy_node()
